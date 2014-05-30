3b451cc4161b6ff1b1562cba41fcf910c69ea78f
JonasHans <jonas.vanoenen@gmail.com>
2014-05-30 18:27:08 +0200

    idle game beginning

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var cookies = 0;
+var food = 0;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+function foodClick(number){
+    food = food + number;
+    document.getElementById("food").innerHTML =food;
 };
 
-var cursors = 0;
+var workers = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+function buyWorker(){
+    var workersCost = Math.floor(10 * Math.pow(1.1,workers));     //works out the cost of this cursor
+    if(food >= workersCost){                                   //checks that the player can afford the cursor
+        workers = workers + 1;                                   //increases number of cursors
+    	food = food - workersCost;                          //removes the cookies spent
+        document.getElementById('workers').innerHTML = workers;  //updates the number of cursors for the user
+        document.getElementById('food').innerHTML = food;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,workers));       //works out the cost of the next cursor
+    document.getElementById('workersCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 	
-	cookieClick(cursors);
+	foodClick(workers);
 	
 }, 1000);
